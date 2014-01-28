49cf2dc20003109d0b69107bd0ec3077aac60baa
vanrails <rv28@waikato.ac.nz>
2014-01-29 12:27:41 +1300

    added creator method to posts & comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base;
   belongs_to :user
+  belongs_to :creator, :foreign_key => 'user_id', :class_name => 'User'
   has_many :comments
   has_and_belongs_to_many :categories
 end
